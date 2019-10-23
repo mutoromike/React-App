5df009c9cf8ff626fd21e9d29eaf6e95f2176fde
strongestavenger <mikemutoro@gmail.com>
2019-10-23 12:58:46 +0300

    commit unstaged changes

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -5,6 +5,7 @@ function HomePage() {
         <div className="jumbotron">
             <h1> Machine Learning and IOT </h1>
             <p> Here we will talk about machine learning and IOT </p>
+            <a href="/about">About</a>
         </div>
     )
 }
