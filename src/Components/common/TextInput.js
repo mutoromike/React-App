1a27cfc2dc3a2e7a2eaa7e64554d5255032c0f72
strongestavenger <mikemutoro@gmail.com>
2019-12-02 13:49:27 +0300

    mudularize forms

--- src/Components/common/TextInput.js
+++ src/Components/common/TextInput.js
@@ -1,5 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function TextInput(props) {
-
+    let wrapperClass = "form-group"
+    if (props.error.length > 0) {
+        wrapperClass += " has-error"
+    }
+    return (
+        <div className={wrapperClass}>
+            <label htmlFor={props.id}>{props.label}</label>
+            <div className="field">
+                <input
+                    id={props.id}
+                    type="text"
+                    name={props.name}
+                    onChange={props.onChange}
+                    className="form-control"
+                    value={props.course.title}
+                />
+            </div>
+            {/* implementing logical AND below: */}
+            {props.error && <div className="alert alert-danger">{props.error}</div>}
+        </div>
+    )
 }
+
+TextInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    error: PropTypes.string,
+    value: PropTypes.string
+}
+
+TextInput.defaultProps = {
+    error: ""
+}
+export default TextInput
